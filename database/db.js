bb910b2322288af7273725c7bf835df810b03157
Uday-Dixit <udaydixit420@gmail.com>
2026-04-08 15:23:06 +0530

    Database module added

---
+++ database/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/loadbalancer");
+
+const serverSchema = new mongoose.Schema({
+  url: String,
+  status: String,
+  connections: { type: Number, default: 0 },
+  lastChecked: Date,
+});
+
+module.exports = mongoose.model("Server", serverSchema);
